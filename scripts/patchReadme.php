92309e7df557d96cd993b11c77ad2b4183b6644a
JoshyPHP <s9e.dev@gmail.com>
2018-01-05 04:51:36 +0100

    Added helper script [ci skip]

---
+++ scripts/patchReadme.php
@@ -0,0 +1,24 @@
+#!/usr/bin/php
+<?php
+
+include __DIR__ . '/../vendor/autoload.php';
+
+$filepath = __DIR__ . '/../README.md';
+file_put_contents(
+	$filepath,
+	preg_replace_callback(
+		'((```php)(.*?)(```\\s+```\\s+)(.*?)(\\s+```))s',
+		'patchOutput',
+		file_get_contents($filepath)
+	)
+);
+
+function patchOutput($m)
+{
+	unset($m[0]);
+	ob_start();
+	eval($m[2]);
+	$m[4] = ob_get_clean();
+
+	return implode('', $m);
+}
