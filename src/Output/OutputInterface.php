ebdc1ca252dd890626ce9f9cdb40e31bbebbf965
JoshyPHP <s9e.dev@gmail.com>
2022-02-09 14:31:34 +0100

    Updated file headers

--- src/Output/OutputInterface.php
+++ src/Output/OutputInterface.php
@@ -2,7 +2,7 @@
 
 /**
 * @package   s9e\RegexpBuilder
-* @copyright Copyright (c) 2016-2021 The s9e authors
+* @copyright Copyright (c) 2016-2022 The s9e authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\RegexpBuilder\Output;
