cddd0090953b7fa377b776e48c4710f97fa28415
JoshyPHP <s9e.dev@gmail.com>
2023-12-09 19:34:39 +0100

    WiP

--- tests/CostEstimatorTest.php
+++ tests/CostEstimatorTest.php
@@ -2,24 +2,21 @@
 
 namespace s9e\RegexpBuilder\Tests;
 
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use s9e\RegexpBuilder\CostEstimator;
 
-/**
-* @covers s9e\RegexpBuilder\CostEstimator
-*/
+#[CoversClass('s9e\RegexpBuilder\CostEstimator')]
 class CostEstimatorTest extends TestCase
 {
-	/**
-	* @dataProvider getEstimateStringTests
-	*/
+	#[DataProvider('getEstimateStringTests')]
 	public function testEstimateString(int $cost, array $string)
 	{
 		$this->assertEquals($cost, (new CostEstimator)->estimateString($string));
 	}
 
-	public static function getPropertiesTests()
+	public static function getEstimateStringTests()
 	{
 		return [
 			// B
@@ -29,46 +26,5 @@ class CostEstimatorTest extends TestCase
 			// BB\n
 			[4, [66, 66, 10]],
 		];
-	}
-
-	/**
-	* @testdox Single character expressions are mapped to the character's codepoint directly if possible
-	* @dataProvider getSingleCharacterTests
-	*/
-	public function testSingleCharacter(string $expr, int $value)
-	{
-		$meta  = new Meta;
-		$meta->set('x', $expr);
-
-		$this->assertEquals(['x' => $value], $meta->getInputMap());
-	}
-
-	public static function getSingleCharacterTests()
-	{
-		return [
-			['b',   ord('b')],
-			['\\.', ord('.')],
-			['\\b', -1 * (1 << count((new ReflectionClass(Meta::class))->getConstants()))],
-		];
-	}
-
-	/**
-	* @testdox Constructor accepts an iterable map
-	*/
-	public function testConstructor()
-	{
-		$meta = new Meta(['x' => '\\b']);
-
-		$this->assertArrayHasKey('x', $meta->getInputMap());
-	}
-
-	/**
-	* @testdox Constructor accepts numbers as keys
-	*/
-	public function testConstructorNumbersAsKeys()
-	{
-		$meta = new Meta(['0' => '\\b']);
-
-		$this->assertArrayHasKey('0', $meta->getInputMap());
 	}
 }
